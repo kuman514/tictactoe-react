f76035490cb8345a88f44de1c2f5915f9bb45e73
kumankoishi.py <7iggif@naver.com>
2021-05-10 15:10:48 +0900

    Show board buttons

---
+++ src/components/Board.tsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+interface BoardState {
+  tiles: number[][],
+  turn: number
+}
+
+interface BoardProps {
+  // Not defined yet
+}
+
+class Board extends Component<BoardProps, BoardState> {
+  public constructor(props: BoardProps) {
+    super(props);
+    this.state = {
+      tiles: [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+      ],
+      turn: 1
+    };
+  }
+  
+  public render(): JSX.Element {
+    return (
+      <div className="Board">
+        {this.showTiles()}
+      </div>
+    );
+  }
+
+  private showTiles(): JSX.Element {
+    let items: JSX.Element[] = [];
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        items.push(
+          <button key={`${i}${j}`}>
+            {this.state.tiles[i][j]}
+          </button>
+        );
+      }
+    }
+    return (<div className="Tiles">{items}</div>);
+  }
+}
+
+export default Board;
